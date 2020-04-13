bc5edd1c9b60702aa4107e9e98b4fd9f98a77bf1
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2020-04-13 14:54:16 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # logserver
-Simple web server that logs incoming requests
+Web server that logs incoming requests
+## Usage
+```
+python3 logserver.py [port]
+```
+By default, the server listens on port 8080.
+If the port option is provided the server listens on the given port.
---
+++ logserver.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_headers()
+        self.wfile.write("<html><body><h1>GET request</h1><p>{}</p></body></html>".format(self.path).encode('utf-8'))
+
+    def do_HEAD(self):
+        logging.info("HEAD request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_headers()
+        self.wfile.write("<html><body><h1>HEAD request</h1><p>{}</p></body></html>".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                     str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_headers()
+        self.wfile.write(
+            "<html><body><h1>POST request</h1><p>{}</p><p>{}</p></body></html>".format(
+                self.path, post_data).encode('utf-8')
+        )
+
+
+def run(port=8080):
+    server_address = ('', port)
+    logserver = HTTPServer(server_address, RequestHandler)
+    logging.info("Starting logserver on port {}...\n".format(port))
+    try:
+        logserver.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    logging.info("Stopping logserver...\n")
+    logserver.server_close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("port", nargs='?', type=int, default=8080, help="TCP port to listen on.")
+    args = parser.parse_args()
+    run(port=args.port)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(message)s', level=logging.INFO)
+    main()
